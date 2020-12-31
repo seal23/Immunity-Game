c650cb097f4490297e78e5c0a19cbea000b24e23
hautran2705 <hautran2705@gmail.com>
2020-12-31 10:48:21 +0700

    Active button NewGame

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public Button newGameBT, continueBT, optionBT, exitBT;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        newGameBT.onClick.AddListener(newGame);
+        continueBT.onClick.AddListener(continueGame);
+        optionBT.onClick.AddListener(optionGame);
+        exitBT.onClick.AddListener(exitGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void newGame(){
+        var parameters = new LoadSceneParameters(LoadSceneMode.Single);
+        SceneManager.LoadScene("Village v0.1");
+    }
+
+    void continueGame(){
+         Debug.Log("Button continue click");
+    }
+
+    void optionGame(){
+         Debug.Log("Button option click");
+    }
+
+    void exitGame(){
+         Debug.Log("Button exit click");
+    }
+}
